edbd66af0b2ef46aa53dc17a7785d4bb9d74d0b1
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2018-07-06 15:51:30 +0300

    init truffle project

---
+++ truffle.js
@@ -0,0 +1,33 @@
+const ganache = require('ganache-core');
+
+module.exports = {
+    networks: {
+        ganache: {
+            network_id: '*', // eslint-disable-line camelcase
+            provider: ganache.provider({
+                total_accounts: 5, // eslint-disable-line camelcase
+                default_balance_ether: 1000000000, // eslint-disable-line camelcase
+                mnemonic: 'mywish',
+                time: new Date('2017-10-10T15:00:00Z'),
+                debug: false,
+                // ,logger: console
+            }),
+        },
+        localhost: {
+            host: 'localhost',
+            port: 8545,
+            network_id: '*', // eslint-disable-line camelcase
+        },
+    },
+    solc: {
+        optimizer: {
+            enabled: true,
+            runs: 200,
+        },
+    },
+    network: 'ganache',
+    mocha: {
+        bail: true,
+        fullTrace: true,
+    },
+};
