05fe421bdb4415d150151a10676f7a8e8ef5d721
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2018-07-17 16:44:25 +0300

    use SafeMath in hardCapReached

--- template/InvestmentPool.sol
+++ template/InvestmentPool.sol
@@ -86,10 +86,13 @@ contract InvestmentPool is // solium-disable-line lbrace
     //#endif
   }
   //#endif
-
   //#if defined(D_MIN_VALUE_WEI) && D_MIN_VALUE_WEI > 0
+
+  /**
+   * @return is hard cap reached or remains less than MIN_VALUE.
+   */
   function hardCapReached() public view returns (bool) {
-    return weiRaised + D_MIN_VALUE_WEI >= hardCap;
+    return weiRaised.add(minInvestment) >= hardCap;
   }
   //#endif
 }
