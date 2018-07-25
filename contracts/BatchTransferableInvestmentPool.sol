96960b9a66a96505828f74e647ad0810e7cc5f1d
Maksim Strenk <lonelyandpsychopatic@gmail.com>
2018-07-25 14:45:15 +0300

    move owner check inside main if condition, prevent investor address duplicates

--- contracts/BatchTransferableInvestmentPool.sol
+++ contracts/BatchTransferableInvestmentPool.sol
@@ -5,8 +5,6 @@ import "./BaseInvestmentPool.sol";
 
 contract BatchTransferableInvestmentPool is BaseInvestmentPool {
 
-  //mapping(address => address) private investors;
-
   address[] public investors;
 
   function transferToAddressesFromPage(uint _index) external nonReentrant onlyOwner {
@@ -24,27 +22,32 @@ contract BatchTransferableInvestmentPool is BaseInvestmentPool {
       address currentInvestor = investors[i + (_index * 100)];
       uint tokenAmount = investments[currentInvestor].mul(tokenAmountMultiplex).sub(tokensWithdrawnByInvestor[currentInvestor]);
       if (investments[currentInvestor] != 0 && tokenAmount != 0) {
-        super._transferTokens(currentInvestor, tokenAmount);
-        tokensWithdrawnByInvestor[currentInvestor] = tokensWithdrawnByInvestor[currentInvestor].add(tokenAmount);
+        ERC20Basic(tokenAddress).transfer(currentInvestor, tokenAmount);
+        tokensWithdrawn += tokenAmount;
+
+        tokensWithdrawnByInvestor[currentInvestor] += tokenAmount;
         emit WithdrawTokens(currentInvestor, tokenAmount);
+
+        if (currentInvestor == owner && rewardPermille != 0) {
+          uint ownerTokenAmount = _getRewardTokenAmount();
+          if (isFinalized && (ownerTokenAmount != 0)) {
+            ERC20Basic(tokenAddress).transfer(owner, ownerTokenAmount);
+            tokensWithdrawn += ownerTokenAmount;
+
+            rewardWithdrawn += ownerTokenAmount;
+            emit WithdrawReward(owner, ownerTokenAmount);
+          }
+        }
       } else {
         continue;
-      }
-      if (currentInvestor == owner && rewardPermille != 0) {
-        uint ownerTokenAmount = _getRewardTokenAmount();
-        if (isFinalized && (ownerTokenAmount != 0)) {
-          super._transferTokens(owner, ownerTokenAmount);
-          rewardWithdrawn = rewardWithdrawn.add(ownerTokenAmount);
-          emit WithdrawReward(owner, ownerTokenAmount);
-        } else {
-          continue;
-        }
       }
     }
   }
 
   function _preValidateInvest(address _beneficiary, uint _amount) internal {
     super._preValidateInvest(_beneficiary, _amount);
-    investors.push(_beneficiary);
+    if (investments[_beneficiary] == 0) {
+      investors.push(_beneficiary);
+    }
   }
 }
